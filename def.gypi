a15417f304e070121e34272eaedf1e51fce5f1fd
mbusenitz <mbusenitz@gmail.com>
2015-02-25 22:00:11 -0800

    master first pass

---
+++ def.gypi
@@ -0,0 +1,6 @@
+{
+	'target_defaults': {
+		'sources': [
+		]
+	}
+}
---
+++ test/src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include <plank/src/test.h>
+
+int main(int argc, const char * argv[])
+{
+	ASSERT(1);
+	std::cout << "hello test cases" << std::endl;
+	return 0;
+}
